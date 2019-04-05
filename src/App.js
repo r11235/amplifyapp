35ebd42da77558d06369731f8a7198e756eea7e6
Rachel <math3point14@gmail.com>
2019-04-04 17:29:15 -0700

    Added links to more tutorials in src/App.js and edited src/App.css

--- src/App.js
+++ src/App.js
@@ -9,16 +9,54 @@ class App extends Component {
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
-            Hello World!
+            Learn about ReactJS, a JavaScript library for building applications:
           </p>
+
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://en.wikipedia.org/wiki/React_(JavaScript_library)"
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            Introduction to React
           </a>
+
+          <a
+            className="App-link"
+            href="https://www.w3schools.com/whatis/whatis_react.asp"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Quickstart Tutorial
+          </a>
+
+          <a
+            className="App-link"
+            href="https://www.tutorialspoint.com/reactjs/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            ReactJS Tutorial
+          </a>
+          
+          <a
+            className="App-link"
+            href="https://reactjs.org/tutorial/tutorial.html"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React at reactjs.org
+          </a>
+
+          <a
+            className="App-link"
+            href="https://www.udemy.com/courses/search/?src=ukw&q=react&p=1&price=price-free"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Free Online Classes at Udemy
+          </a>
+
         </header>
       </div>
     );
