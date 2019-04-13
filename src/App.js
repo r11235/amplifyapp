eb52fe527120ef00db93d6ce94b731b61ff8532e
Rachel <math3point14@gmail.com>
2019-04-13 14:53:23 -0700

    Edited src/App.js and src/App.css

--- src/App.js
+++ src/App.js
@@ -6,60 +6,90 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
+          <h3>
             This app was built with React and deployed in the cloud by using the AWS Amplify 
-            Console and integrating with GitHub.  
+            Console and integrating with GitHub. 
+          </h3>
+          <h4>
             Learn about ReactJS, a JavaScript library for building applications:
+          </h4>
+        </header>
+
+        <body>
+          <p>
+            <a
+              className="App-link"
+              href="https://en.wikipedia.org/wiki/React_(JavaScript_library)"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Introduction to React
+            </a>
           </p>
 
-          <a
-            className="App-link"
-            href="https://en.wikipedia.org/wiki/React_(JavaScript_library)"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Introduction to React
-          </a>
+          <p>
+            <a
+              className="App-link"
+              href="https://www.w3schools.com/whatis/whatis_react.asp"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Quickstart Tutorial
+            </a>
+          </p>
 
-          <a
-            className="App-link"
-            href="https://www.w3schools.com/whatis/whatis_react.asp"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Quickstart Tutorial
-          </a>
+          <p>
+           <a
+              className="App-link"
+              href="https://www.tutorialspoint.com/reactjs/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              ReactJS Tutorial
+            </a>
+          </p>
 
-          <a
-            className="App-link"
-            href="https://www.tutorialspoint.com/reactjs/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            ReactJS Tutorial
-          </a>
-          
-          <a
-            className="App-link"
-            href="https://reactjs.org/tutorial/tutorial.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React at reactjs.org
-          </a>
+          <p>
+            <a
+              className="App-link"
+              href="https://reactjs.org/tutorial/tutorial.html"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Learn React at reactjs.org
+            </a>
+          </p>
 
-          <a
-            className="App-link"
-            href="https://www.udemy.com/courses/search/?src=ukw&q=react&p=1&price=price-free"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Free Online Classes at Udemy
-          </a>
+          <p>
+            <a
+              className="App-link"
+              href="https://www.udemy.com/courses/search/?src=ukw&q=react&p=1&price=price-free"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              Free Online Classes at Udemy
+            </a>
+          </p>
 
-        </header>
+          <p>
+            <a
+              className="App-link"
+              href="https://aws.amazon.com/getting-started/tutorials/deploy-react-app-cicd-amplify/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              AWS Amplify React Tutorial
+            </a>
+          </p>
+
+          <div>
+            <h2> . </h2>
+          </div>
+        </body>
+
       </div>
     );
   }
